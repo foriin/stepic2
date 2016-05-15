6714d28df51c3fd3d72ac2a73ca58163b6a712d1
A <foriin@users.noreply.github.com>
2016-05-15 17:49:04 +0400

    Update hello.py

--- web/hello.py
+++ web/hello.py
@@ -1,4 +1,4 @@
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    url = environ['QUERY_STRING'][2:]
+    url = environ['QUERY_STRING']
     return url.replace('&', '\n')
