79b25c0dd72b44371610e174e3f43819ce92914e
foriin <liartom@mail.ru>
2016-05-15 14:56:41 +0000

    passed this shit somehow

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    url = environ['QUERY_STRING']
-    return url.replace('&', '\n')
+    url = env['QUERY_STRING']
+    url = url.split('&')
+    url2 = [x + '\n' for x in url]
+    return url2
--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
 sudo /etc/init.d/gunicorn restart
+gunicorn -c /etc/gunicorn.d/hello.py hello
